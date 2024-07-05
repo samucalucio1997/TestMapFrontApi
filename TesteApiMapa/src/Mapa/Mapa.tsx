96951e073994388b3ecbb000d362dc3f5d29327c
Samuel Lúcio Moreira de Farias <20222014040004@ifrn.local>
2024-07-05 14:26:18 -0300

    searchBox dependency

--- TesteApiMapa/src/Mapa/Mapa.tsx
+++ TesteApiMapa/src/Mapa/Mapa.tsx
@@ -1,4 +1,5 @@
 import tt from '@tomtom-international/web-sdk-maps';
+import x from '@tomtom-international/web-sdk-plugin-searchbox'
 import { useEffect } from 'react';
 
 function Mapa() {
@@ -7,10 +8,24 @@ function Mapa() {
         const mapContainer = document.getElementById('map');
         if (mapContainer) {
             tt.map({
-                key: 'chave-api',
+                key: 'o0JA6AFfGiL8GFelg6TnTtH0zhwyfH8g',
                 container: mapContainer,
             });
         }
+        var options = {
+            searchOptions: {
+                key: 'o0JA6AFfGiL8GFelg6TnTtH0zhwyfH8g',
+                language: "en-GB",
+                limit: 5,
+              },
+              autocompleteOptions: {
+                key: 'o0JA6AFfGiL8GFelg6TnTtH0zhwyfH8g',
+                language: "en-GB",
+              },
+        }
+        var searc; 
+    
+
     }, []);
 
     return (
