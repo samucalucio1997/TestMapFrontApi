eb049601ba3e544ca9d6ccb7bc862f251cf18079
samucalucio1997 <samucalucio@hotmail.com>
2024-07-06 19:08:40 -0300

    subido a renderização da rota(por enquanto estática) e o marcador tbm

--- TesteApiMapa/src/Mapa/Mapa.tsx
+++ TesteApiMapa/src/Mapa/Mapa.tsx
@@ -1,36 +1,61 @@
-import tt from '@tomtom-international/web-sdk-maps';
-import x from '@tomtom-international/web-sdk-plugin-searchbox'
+import tt, { Layer, LngLatLike } from '@tomtom-international/web-sdk-maps';
+import { services } from '@tomtom-international/web-sdk-services';
+import SearchBox from '@tomtom-international/web-sdk-plugin-searchbox';
 import { useEffect } from 'react';
 
 function Mapa() {
 
     useEffect(() => {
         const mapContainer = document.getElementById('map');
-        if (mapContainer) {
-            tt.map({
-                key: 'o0JA6AFfGiL8GFelg6TnTtH0zhwyfH8g',
-                container: mapContainer,
-            });
-        }
+        const compoMap = tt.map({
+            key: 'chave-api',
+            container: mapContainer? mapContainer : '',
+        });
         var options = {
             searchOptions: {
-                key: 'o0JA6AFfGiL8GFelg6TnTtH0zhwyfH8g',
-                language: "en-GB",
+                key: 'chave-api',
+                language: "pt-BR",
                 limit: 5,
               },
               autocompleteOptions: {
-                key: 'o0JA6AFfGiL8GFelg6TnTtH0zhwyfH8g',
-                language: "en-GB",
+                key: 'chave-api',
+                language: "pt-BR",
               },
         }
-        var searc; 
-    
+        const searc = new SearchBox(services,options); 
+        const searchScreen = searc.getSearchBoxHTML();
+        mapContainer?.appendChild(searchScreen);        
+        
+        services.calculateRoute({
+            key: 'chave-api',
+            locations: '-43.1729,-22.9068:-46.6333,-23.5505'
+        }).then((routeData) => {
+            const geojson = routeData.toGeoJson();
+            const routeLayer = {
+                id: 'route',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: geojson
+                },
+                paint: {
+                    'line-color': '#FF0000',
+                    'line-width': 6
+                }
+            };
+            compoMap.addLayer(routeLayer as tt.LineLayer);
+            const coordinates = geojson.features[0].geometry.coordinates;
+            const bounds = coordinates.reduce((b, coord) => b.extend(coord as LngLatLike), new tt.LngLatBounds(coordinates[0] as LngLatLike, coordinates[0] as LngLatLike));
+            compoMap.fitBounds(bounds, { padding: 50 });
+        });
+
+        new tt.Marker().setLngLat([-43.0759, -22.9065]).addTo(compoMap);
 
     }, []);
 
     return (
         <>
-            <div id="map"></div>
+            <div id="map" style={{ width: '100%', height: '500px' }}></div>
         </>
     )
 }
